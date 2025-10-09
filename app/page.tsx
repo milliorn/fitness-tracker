74e629cdaac32c20d27e262fc0a16033f19fa5b5
Scott Milliorn <scottmilliorn@gmail.com>
2025-10-08 22:04:10 -0700

    style(ui): add spacing and layout adjustments for Login and Register buttons in ButtonGroup

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,25 @@ import ButtonGroup from "@mui/material/ButtonGroup";
 export default function Home() {
   return (
     <ButtonGroup
+      id="home-login-register-buttons"
       variant="outlined"
       aria-label="login and register buttons"
       sx={{
+        // overrides inline-flex for consistent spacing control
+        alignItems: "center",
+        borderRadius: "9999px",
+        display: "flex",
+        justifyContent: "center",
         "& .MuiButton-root": {
           borderColor: "white",
-          borderRadius: '9999px',
+          borderRadius: "9999px",
           color: "white",
-          px: 2
-
+          px: 3,
+        },
+        // CSS selector that targets every .MuiButton-root except the last one inside the 
+        // ButtonGroup to add spacing between the buttons, but not after the last one.
+        "& .MuiButton-root:not(:last-of-type)": {
+          marginRight: "12px",
         },
       }}
     >
