281f084ee21d16f76dcc7bc42d753b7a140d24c0
Scott Milliorn <scottmilliorn@gmail.com>
2025-10-25 04:13:42 -0700

    chore(layout): remove Roboto font imports from RootLayout
    - app/layout.tsx: delete @fontsource/roboto 300/400/500/700 imports
    - fonts are handled via MUI/theme pipeline; no visual change intended

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,5 @@
 import type { Metadata } from "next";
 
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import MuiTheme from "./MuiTheme";
 
